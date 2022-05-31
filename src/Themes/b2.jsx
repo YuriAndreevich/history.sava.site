c94dc1d6c37345798b46b8934f0ce11d80d70af0
YuriAndreevich <YuriAndreev11ch@gmail.com>
2022-05-31 22:00:27 +0300

    1

---
+++ src/Themes/b2.jsx
@@ -0,0 +1,30 @@
+
+import '../App.css';
+
+function b1() {
+  return (
+    <div className='bg-fon'>
+    <div className="wrapper">
+        <h1>Билет 2. История Беларуси</h1>
+        <details>
+            <summary>
+                <h3>Назв</h3>
+            </summary><b>1</b>текст
+
+        </details>
+        <details>
+            <summary> <h3>названия</h3></summary>
+            текст
+ </details>
+
+        <details>
+            <summary>
+                <h3>назв</h3>
+            </summary>текст
+        </details>
+    </div>
+    </div>
+  );
+}
+
+export default b1;
