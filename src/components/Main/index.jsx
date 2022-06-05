878ccbfe60090a83819e562f8031e4472df4f873
YuriAndreevich <YuriAndreev11ch@gmail.com>
2022-06-05 13:46:10 +0300

    1

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import p from '../../img/1.pdf'
-import s from './main.module.css'
+import s from './main.module.scss'
 
 const Main = () => {
     return(
 <div className={s.main__cover}>
 <br></br><br></br><br></br><p className={s.main__text}>Экзаменационные билеты по Истории Беларуси</p>
 
-<details style={s.details}><summary>Онлайн учебник</summary><embed src={p} width="700" height="800" /></details>
-
+<div className={s.main__search}><details className={s.main__details}><summary>Поиск по датам</summary></details>
+    <details className={s.main__details} ><summary>Онлайн учебник</summary><embed src={p} width="700" height="800" /></details>
+</div>
 </div>
 
     )
