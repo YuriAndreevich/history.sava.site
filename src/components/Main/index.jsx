fd0cbc63f011a5bfbf9fd02a3e811eb713df8d14
YuriAndreevich <YuriAndreev11ch@gmail.com>
2022-06-07 21:23:19 +0300

    1

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,14 +3,14 @@ import p from '../../img/1.pdf'
 import './main.scss'
 
 const Main = () => {
-    return(
-<div className='main__cover'>
-<p className='main__text'>Экзаменационные билеты по Истории Беларуси</p>
+    return (
+        <div className='main__cover'>
+            <p className='main__text'>Экзаменационные билеты по Истории Беларуси</p>
 
-<div className='main__search'>
-    <details className={'main__details'} ><summary><p style={{textAlign:'center'}}>Онлайн учебник</p></summary><iframe title="Онлайн учебник" src={p} width="700" height="800" /></details>
-</div>
-</div>
+            <div className='main__search'>
+                <details className={'main__details'} ><summary><p style={{ textAlign: 'center' }}>Онлайн учебник</p></summary><iframe title="Онлайн учебник" src={p} width="700" height="800" /></details>
+            </div>
+        </div>
 
     )
 }
--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -8,13 +8,13 @@ const Menu = ({ header, items, active, setActive }) => {
             <div className="blur">
                 <div className="menu__content">
                     <ul>
-                    <li><Link to='/'>Главная</Link></li>
-                    <li><Link to='/search'>Поиск по датам</Link></li>
-                    <hr style={{width:'100hv'}}/>
-                         {items.map((item, index) =>
-                        <li key={index}>
-                            <Link to={item.href}>{item.naz}</Link>
-                        </li>)}
+                        <li><Link to='/'>Главная</Link></li>
+                        <li><Link to='/search'>Поиск по датам</Link></li>
+                        <hr style={{ width: '100hv' }} />
+                        {items.map((item, index) =>
+                            <li key={index}>
+                                <Link to={item.href}>{item.naz}</Link>
+                            </li>)}
                     </ul>
                 </div>
             </div>
