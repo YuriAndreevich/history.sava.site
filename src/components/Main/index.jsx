855bf916580bd877a66b37da38c0cfe5eb57e85b
AlexSavr <YuriAndreev11ch@gmail.com>
2022-06-14 13:47:42 +0300

    1

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -5,8 +5,7 @@ import './main.scss'
 const Main = () => {
     return (
         <div className='main__cover'>
-            <p className='main__text'>Экзаменационные билеты по Истории Беларуси</p>
-
+            <p className='main__text'>Экзаменационные билеты по «Истории Беларуси»</p>
             <div className='main__search'>
                 <details className={'main__details'} ><summary><p style={{ textAlign: 'center' }}>Онлайн учебник</p></summary><iframe title="Онлайн учебник" src={p} width="700" height="800" /></details>
             </div>
