0f2230b741fc6d11c3596b8c38716e6c70e46db4
YuriAndreevich <YuriAndreev11ch@gmail.com>
2022-06-16 00:12:16 +0300

    1

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -9,18 +9,14 @@ function Quiz() {
             <div className='quiz' style={{ marginTop: '60px' }}>
                 <p> Историческое личности</p>
                 <hr />
-                <Test /><Test />
                 <Test />
-                <Test />
-                <Test />            </div>
-
+                <div className="card"></div><div className="card"></div><div className="card"></div><div className="card"></div>
+            </div>
             <div className='quiz'>
-            <p> Историческое памятники</p>
-            <hr />
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
+                <p> Историческое памятники</p>
+                <hr />
+  <div className="card"></div>
+  <div className="card"></div>
             </div>
         </>
     )
